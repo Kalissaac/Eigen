2c0a39c09b217b59f14eddf2ceffc0ecb70eb3d3
Kalissaac <kalissaac@protonmail.com>
2021-08-07 16:01:29 -0700

    Initial Commit

---
+++ Eigen/EigenApp.swift
@@ -0,0 +1,16 @@
+//
+// EigenApp.swift
+// Eigen
+//
+        
+
+import SwiftUI
+
+@main
+struct EigenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
