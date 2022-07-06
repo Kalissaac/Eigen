582c0308f6b9d2efb8722f077ac1db61e1331672
Kalissaac <kalissaac@protonmail.com>
2022-07-06 15:32:49 -0700

    Try to fix sign out error

--- Eigen/Views/PreferencesView.swift
+++ Eigen/Views/PreferencesView.swift
@@ -28,47 +28,51 @@ struct PreferencesView: View {
                 Toggle("Show room member events", isOn: $matrix.showRoomMemberEvents)
             }
             Section("Devices") {
-                ForEach(Array(matrix.session.crypto.devices(forUser: matrix.session.myUserId).values), id: \.deviceId) { device in
-                    Text("\(device.displayName) (\(device.deviceId))")
+                if matrix.session.crypto != nil {
+                    ForEach(Array(matrix.session.crypto.devices(forUser: matrix.session.myUserId).values), id: \.deviceId) { device in
+                        Text("\(device.displayName) (\(device.deviceId))")
+                    }
                 }
             }
             Section("Encryption") {
                 Text("Session ID: \(matrix.session.myDeviceId ?? "Unknown")")
-                Text("Cross signing enabled: \(String(matrix.session.crypto.crossSigning.canCrossSign))")
-                Section("Recovery") {
-                    Text("Recovery enabled: \(String(matrix.session.crypto.recoveryService.hasRecovery()))")
-                    Text("Recovery by passphrase: \(String(matrix.session.crypto.recoveryService.usePassphrase()))")
-                    Text("# secrets in recovery: \(matrix.session.crypto.recoveryService.secretsStoredInRecovery().count)")
-                    Text("# secrets locally: \(matrix.session.crypto.recoveryService.secretsStoredLocally().count)")
-                    SecureField("Recovery password", text: $crosssignpass)
-                    Button("Restore from recovery", action: {
-                        do {
-                            let privkey = try matrix.session.crypto.recoveryService.privateKey(fromRecoveryKey: crosssignpass)
-                            matrix.session.crypto.recoveryService.recoverSecrets(nil, withPrivateKey: privkey, recoverServices: true) { recoveryresult in
-                                print(recoveryresult.secrets)
-                            } failure: { err in
-                                print(err)
+                if matrix.session.crypto != nil {
+                    Text("Cross signing enabled: \(String(matrix.session.crypto.crossSigning.canCrossSign))")
+                    Section("Recovery") {
+                        Text("Recovery enabled: \(String(matrix.session.crypto.recoveryService.hasRecovery()))")
+                        Text("Recovery by passphrase: \(String(matrix.session.crypto.recoveryService.usePassphrase()))")
+                        Text("# secrets in recovery: \(matrix.session.crypto.recoveryService.secretsStoredInRecovery().count)")
+                        Text("# secrets locally: \(matrix.session.crypto.recoveryService.secretsStoredLocally().count)")
+                        SecureField("Recovery password", text: $crosssignpass)
+                        Button("Restore from recovery", action: {
+                            do {
+                                let privkey = try matrix.session.crypto.recoveryService.privateKey(fromRecoveryKey: crosssignpass)
+                                matrix.session.crypto.recoveryService.recoverSecrets(nil, withPrivateKey: privkey, recoverServices: true) { recoveryresult in
+                                    print(recoveryresult.secrets)
+                                } failure: { err in
+                                    print(err)
+                                }
+                            } catch {
+
                             }
-                        } catch {
-                            
-                        }
-                    })
-                }
-                Text("Curve25519 key: \(matrix.session.crypto.deviceCurve25519Key)")
-                Text("Ed25519 key: \(matrix.session.crypto.deviceEd25519Key)")
-                Text("Key backup enabled: \(String(matrix.session.crypto.backup.enabled))")
-                Text("Can send messages to unverified sessions: \(String(!matrix.session.crypto.globalBlacklistUnverifiedDevices))")
-                Spacer()
-                Section("Import keys") {
-                    TextEditor(text: $keysimport)
-                        .foregroundColor(.secondary)
-                    SecureField("Password", text: $keyspass)
-                    Button("Import", action: {
-                        matrix.session.crypto.importRoomKeys(keysimport.data(using: .utf8), withPassword: keyspass) { _, _ in
-                        } failure: { err in
-                            print(err!)
-                        }
-                    })
+                        })
+                    }
+                    Text("Curve25519 key: \(matrix.session.crypto.deviceCurve25519Key)")
+                    Text("Ed25519 key: \(matrix.session.crypto.deviceEd25519Key)")
+                    Text("Key backup enabled: \(String(matrix.session.crypto.backup.enabled))")
+                    Text("Can send messages to unverified sessions: \(String(!matrix.session.crypto.globalBlacklistUnverifiedDevices))")
+                    Spacer()
+                    Section("Import keys") {
+                        TextEditor(text: $keysimport)
+                            .foregroundColor(.secondary)
+                        SecureField("Password", text: $keyspass)
+                        Button("Import", action: {
+                            matrix.session.crypto.importRoomKeys(keysimport.data(using: .utf8), withPassword: keyspass) { _, _ in
+                            } failure: { err in
+                                print(err!)
+                            }
+                        })
+                    }
                 }
             }
         }
