693d124778b8f5b0290f306b63b8f7cab3b06c38
Kalissaac <kalissaac@protonmail.com>
2023-04-04 15:08:26 -0700

    Overhault account management

--- Eigen/Shared/MatrixModel.swift
+++ Eigen/Shared/MatrixModel.swift
@@ -22,8 +22,11 @@ enum MatrixAuthenticationStatus {
     case error
 }
 
+let KEYCHAIN_SERVICE = "Eigen"
 
 class MatrixModel: ObservableObject {
+    typealias MatrixAccount = [String : String]
+
     @Published var session = MXSession()
     @Published var store = MXFileStore()
     @Published var authenticationStatus: MatrixAuthenticationStatus = .loading
@@ -34,6 +37,7 @@ class MatrixModel: ObservableObject {
     }
 
     init() {
+        MatrixModel.migrateCredentials()
         if MatrixModel.credentialsAreIntact() {
             login()
         } else {
@@ -41,77 +45,144 @@ class MatrixModel: ObservableObject {
         }
     }
 
-    func login() {
+    func login(withUsername username: String? = nil) {
         let defaults = UserDefaults.standard
-        let homeserver = defaults.string(forKey: "homeserver") ?? "https://matrix.org"
-        let username = defaults.string(forKey: "username")!
-        let accessTokenData = try! Keychain.readPassword(service: homeserver, account: username)
-        let accessToken = String(data: accessTokenData, encoding: .utf8)!
+        guard let accounts = defaults.array(forKey: "accounts") as? [MatrixAccount] else { return }
+        guard let username = username ?? defaults.string(forKey: "lastUsername") ?? accounts.first?["username"] else { return }
+        guard let accountMetadata = accounts.first(where: { $0["username"] == username }) else { return }
+        guard let homeserver = accountMetadata["homeserver"] else { return }
+        guard let deviceId = accountMetadata["deviceId"] else { return }
+        guard let accessTokenData = try? Keychain.readPassword(service: KEYCHAIN_SERVICE, account: username) else { return }
+        guard let accessToken = String(data: accessTokenData, encoding: .utf8) else { return }
+
         let credentials = MXCredentials(homeServer: homeserver, userId: username, accessToken: accessToken)
-        credentials.deviceId = defaults.string(forKey: "deviceId")!
+        credentials.deviceId = deviceId
         login(withCredentials: credentials)
     }
 
     func login(withCredentials credentials: MXCredentials, savingToKeychain saveToKeychain: Bool = false) {
+        session.close()
         let restClient = MXRestClient(credentials: credentials, unrecognizedCertificateHandler: nil)
         guard let _session = MXSession(matrixRestClient: restClient) else {
             authenticationStatus = .error
             return
         }
         session = _session
+        store = MXFileStore(credentials: credentials)
         authenticationStatus = .authenticated
 
+        let defaults = UserDefaults.standard
+        defaults.set(credentials.userId, forKey: "lastUsername")
+
         if saveToKeychain {
-            let defaults = UserDefaults.standard
-            defaults.set(credentials.homeServer, forKey: "homeserver")
-            defaults.set(credentials.userId, forKey: "username")
-            try? Keychain.save(password: credentials.accessToken!.data(using: .utf8)!, service: credentials.homeServer!, account: credentials.userId!)
-            defaults.set(credentials.deviceId, forKey: "deviceId")
-            // if saving to keychain, likely initial sync
-            syncStatus = .initialSync
+            guard let username = credentials.userId else { return }
+            guard let homeserver = credentials.homeServer else { return }
+            guard let deviceId = credentials.deviceId else { return }
+
+            let accountData = [
+                "username": username,
+                "homeserver": homeserver,
+                "deviceId": deviceId
+            ]
+
+            var accounts = defaults.array(forKey: "accounts") as? [MatrixAccount] ?? []
+            if let accountIndex = accounts.firstIndex(where: { $0["username"] == username }) {
+                accounts[accountIndex] = accountData
+            } else {
+                accounts.append(accountData)
+            }
+            defaults.set(accounts, forKey: "accounts")
+
+            try? Keychain.save(password: credentials.accessToken!.data(using: .utf8)!, service: KEYCHAIN_SERVICE, account: username)
         }
     }
 
     func logout() {
+        guard let username = session.credentials.userId else { return }
+
+        self.authenticationStatus = .notAuthenticated
+
         let defaults = UserDefaults.standard
-        defaults.removeObject(forKey: "homeserver")
-        defaults.removeObject(forKey: "username")
-        defaults.removeObject(forKey: "deviceId")
-        if let homeserver = session.credentials.homeServer, let username = session.credentials.userId {
-            try? Keychain.deletePassword(service: homeserver, account: username)
+
+        try? Keychain.deletePassword(service: KEYCHAIN_SERVICE, account: username)
+        var accounts = defaults.array(forKey: "accounts") as? [MatrixAccount] ?? []
+        accounts = accounts.filter({ $0["username"] != username })
+        defaults.set(accounts, forKey: "accounts")
+
+        session.logout { _ in
+            if let account = accounts.first {
+                self.login(withUsername: account["username"])
+            }
         }
-        session.logout { _ in }
+    }
+
+    func switchSession(account: MatrixAccount) {
+        login(withUsername: account["username"])
+    }
+
+    func addAccount() {
         authenticationStatus = .notAuthenticated
+        session.close()
     }
 
     static func credentialsAreIntact() -> Bool {
         do {
             let defaults = UserDefaults.standard
 
-            guard let homeserver = defaults.string(forKey: "homeserver") else {
-                throw MatrixCredentialValidationError.invalidHomeserver
-            }
-
-            guard let username = defaults.string(forKey: "username") else {
+            guard var accounts = defaults.array(forKey: "accounts") as? [MatrixAccount] else {
                 throw MatrixCredentialValidationError.invalidUsername
             }
 
-            guard let accessTokenData = try? Keychain.readPassword(service: homeserver, account: username) else {
-                throw MatrixCredentialValidationError.invalidAccessToken
+            if accounts.count == 0 {
+                return false
             }
 
-            guard String(data: accessTokenData, encoding: .utf8) != nil else {
-                throw MatrixCredentialValidationError.invalidAccessToken
+            guard defaults.string(forKey: "lastUsername") != nil else { return false }
+
+            accounts = accounts.filter { account in
+                guard let username = account["username"] else { return false }
+                guard account["homeserver"] != nil else { return false }
+                guard account["deviceId"] != nil else { return false }
+                guard let accessTokenData = try? Keychain.readPassword(service: KEYCHAIN_SERVICE, account: username) else { return false }
+                guard String(data: accessTokenData, encoding: .utf8) != nil else { return false }
+                return true
             }
 
-            guard defaults.string(forKey: "deviceId") != nil else {
-                throw MatrixCredentialValidationError.invalidDeviceId
-            }
+            defaults.set(accounts, forKey: "accounts")
 
             return true
         } catch {
             return false
         }
+    }
+
+    static func migrateCredentials() {
+        let defaults = UserDefaults.standard
+        let username = defaults.string(forKey: "username")
+        defaults.removeObject(forKey: "username")
+        let homeserver = defaults.string(forKey: "homeserver")
+        defaults.removeObject(forKey: "homeserver")
+        let deviceId = defaults.string(forKey: "deviceId")
+        defaults.removeObject(forKey: "deviceId")
+
+        guard let homeserver = homeserver, let username = username else { return }
+        guard let accessTokenData = try? Keychain.readPassword(service: homeserver, account: username) else { return }
+        if (try? Keychain.save(password: accessTokenData, service: KEYCHAIN_SERVICE, account: username)) != nil {
+            try? Keychain.deletePassword(service: homeserver, account: username)
+        }
+        var accounts = defaults.array(forKey: "accounts") as? [MatrixAccount] ?? []
+        accounts.append([
+            "username": username,
+            "homeserver": homeserver,
+            "deviceId": deviceId ?? ""
+        ])
+        defaults.set(accounts, forKey: "accounts")
+        defaults.set(username, forKey: "lastUsername")
+    }
+
+    static func getAccounts() -> [MatrixAccount] {
+        let defaults = UserDefaults.standard
+        return defaults.array(forKey: "accounts") as? [MatrixAccount] ?? []
     }
 }
 
@@ -127,4 +198,11 @@ struct MatrixPreferences {
 
     @UserDefaultsStorage(key: "showRoomIconsInSidebar", defaultValue: false)
     var showRoomIconsInSidebar: Bool
+}
+
+struct MatrixAccount {
+    let username: String
+    let homeserver: String
+    let deviceId: String
+
 }
